1eaa901fee4889365f40bc90eef100148b4fd49b
Kenny F <kfrachelle123@gmail.com>
2020-01-11 21:53:27 +0800

    refactor(parser): merged binary digits in bin expr parsing

--- src/parser/expressions.ts
+++ src/parser/expressions.ts
@@ -162,10 +162,7 @@ export function parseBinaryExpression(
 
     if ((t & prec) + (((t === Token.Exponentiate) as any) << 8) <= minPrec) return left;
 
-    if (
-      ((l & 0b01000000100000000000000000000000) | (t & 0b01000000100000000000000000000000)) >
-      0b01000000000000000000000000000000
-    ) {
+    if (((l | t) & 0b01000000100000000000000000000000) > 0b01000000000000000000000000000000) {
       report(parser, Errors.InvalidCoalescing);
     }
 
